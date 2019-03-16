fbb56fc29bfe8fae28dd0009789a268a598f51c1
Kevin Li <kevinli4321@gmail.com>
2019-03-16 11:24:43 -0400

    Transferred mergesort contents to helper fn.

--- Driver.java
+++ Driver.java
@@ -6,6 +6,6 @@ public class Driver {
     int[] arr2 = {22, 2, 3, 4, 6, 7, 10, 91, 3021, -9, 98421};
     int[] arr3 = {999, 999, 999, 4, 1, 0, 3, 2, 999, 999, 999};
 
-    Merge.mergesort(arr3);
+    Merge.mergesort(arr);
   }
 }
--- Merge.java
+++ Merge.java
@@ -6,8 +6,14 @@ public class Merge {
     if (data.length <= 1)
       return;
 
-    int midIdx = data.length / 2;
+    mergesortH(data, 0, data.length - 1);
+  }
 
+  private static void mergesortH(int[] data, int lo, int hi) {
+    if (lo >= hi)
+      return;
+
+    int midIdx = data.length / 2;
     int[] left; //decided that left is always larger
     int[] right = new int[midIdx];
 
@@ -22,14 +28,11 @@ public class Merge {
       left[i] = data[i];
 
     for (int i = 0; i < right.length; ++i) {
-      if (data.length % 2 == 0)
+      if (data.length % 2 == 0) //copy into right if even-sized array
         right[i] = data[i + midIdx];
 
-      else
+      else //copy into right if odd-sized array
         right[i] = data[i + midIdx + 1];
     }
-
-    System.out.println(Arrays.toString(left));
-    System.out.println(Arrays.toString(right));
   }
 }
