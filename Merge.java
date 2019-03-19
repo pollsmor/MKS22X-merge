98458c04bc948ff8536776c8b6f1f6df7e9ae03f
Kevin Li <kevinli4321@gmail.com>
2019-03-19 19:44:55 -0400

    Added my insertionSort from first sem, let's see what I need to modify

--- Merge.java
+++ Merge.java
@@ -136,4 +136,20 @@ public class Merge {
       ++i;
     }
   }
+
+  //Insertion sort for further optimization of mergesort
+  private static void insertionsort(int[] data, int lo, int hi) {
+    for (int i = 1; i < data.length; ++i) { //Start at 1 b/c we assume 0 is sorted already
+      int valueAtI = data[i]; //To have something to compare with
+      int j = i - 1; //Start from 1 before <?> so that I can loop backwards from it
+      //System.out.println(Arrays.toString(data)); //for testing purposes
+
+      while (j >= 0 && data[j] > valueAtI) { //Keep shifting left, until I find a pos. where valueAtI is larger than j
+        data[j + 1] = data[j]; //Keep shifting everything to the right
+        --j; //Loop backwards b/c looping forwards will be slower with larger data sets
+      }
+
+      data[j + 1] = valueAtI; //We want data[j] but the while loop does a final --i, we add an index to get j
+    }
+  }
 }
